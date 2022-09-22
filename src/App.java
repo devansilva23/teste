2579311944c61d75ed43b04b2137a7dc6945df12
devan <>
2022-09-22 18:56:26 -0300

    first comit

---
+++ src/App.java
@@ -0,0 +1,16 @@
+public class Principal {
+    public static void main(String[] args) throws Exception {
+        System.out.println("ola mundo!");
+
+        Curso c1 = new Curso();
+        c1.setNome("ads");
+
+        Aluno a1 = new Aluno();
+        a1.setCargaHoraria(34);
+
+        Professor p1 = new Professor();
+        p1.setNome("eric");
+        p1.status;
+
+    }
+}
